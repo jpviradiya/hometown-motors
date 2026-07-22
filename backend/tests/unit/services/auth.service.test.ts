15f686baba7c83cb0ead3a52b641c6bf41114f4d
Jeel Viradiya <121432019+jpviradiya@users.noreply.github.com>
2026-07-22 22:36:06 +0530

    test(auth): add role authorization tests

--- backend/tests/unit/services/auth.service.test.ts
+++ backend/tests/unit/services/auth.service.test.ts
@@ -1,6 +1,9 @@
 import { describe, expect, it } from "vitest";
 import { AuthRepository } from "#/repositories";
 import { AuthService } from "#/services";
+import request from "supertest";
+import app from "#/app";
+import { prisma } from "#/lib/prisma";
 
 describe("AuthService", () => {
   it("should hash the password before saving the user", async () => {
@@ -37,6 +40,61 @@ describe("AuthService", () => {
     expect(response).toEqual({
       message: "Login successful",
       token: expect.any(String),
+    });
+  });
+
+  it("should allow admin to access protected route", async () => {
+    await request(app).post("/api/v1/auth/register").send({
+      name: "Admin",
+      email: "admin@test.com",
+      password: "Password@123",
+    });
+
+    await prisma.user.update({
+      where: {
+        email: "admin@test.com",
+      },
+      data: {
+        role: "ADMIN",
+      },
+    });
+
+    const login = await request(app).post("/api/v1/auth/login").send({
+      email: "admin@test.com",
+      password: "Password@123",
+    });
+
+    const response = await request(app)
+      .get("/api/v1/auth/admin")
+      .set("Authorization", `Bearer ${login.body.token}`);
+
+    expect(response.status).toBe(200);
+
+    expect(response.body).toEqual({
+      message: "Access granted",
+    });
+  });
+
+  it("should deny normal user", async () => {
+    await request(app).post("/api/v1/auth/register").send({
+      name: "User",
+      email: "user@test.com",
+      password: "Password@123",
+    });
+
+    const login = await request(app).post("/api/v1/auth/login").send({
+      email: "user@test.com",
+      password: "Password@123",
+    });
+
+    const response = await request(app)
+      .get("/api/v1/auth/admin")
+      .set("Authorization", `Bearer ${login.body.token}`);
+
+    expect(response.status).toBe(403);
+
+    expect(response.body).toEqual({
+      message: "Forbidden",
     });
   });
 });
